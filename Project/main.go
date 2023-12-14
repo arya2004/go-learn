8fd850c658142adbc229ac54f1e9b33fd7d6d0ac
arya2004 <arya20j4@gmail.com>
2023-12-15 03:02:53 +0530

    i/o operatoins

--- Project/main.go
+++ Project/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func Args() {
-	if len(os.Args) > 1 {
-		fmt.Println("HEllo, ", os.Args[1])
-		return
-	} else {
-		fmt.Println("There is no args")
-		return
-	}
-
-}
-
 func main() {
-	Args()
+	var conferrenceName string = "GO conf"
+	const conferrenceTIckets int = 50
+	var remainingTickets int = 50
+	//auto spacing
+	//Variables: var name type = value
+
+	fmt.Printf("conferrenceName type %T\n", conferrenceName)
+
+	fmt.Printf("Welcome to %v booking app\n", conferrenceName)
+	fmt.Println("We have total of", conferrenceTIckets, "and remaining", remainingTickets)
+	fmt.Println("Get tickets here")
+
+	var userName string
+	var userTickets int
+	//input name
+
+	fmt.Scan(&userName)
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
 }
