9b5f703542ccd88e86e2297ca4d41fda7e7f9ead
arya2004 <arya20j4@gmail.com>
2023-12-15 13:51:24 +0530

    conditionals and cases

--- Project/main.go
+++ Project/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	var conferrenceName string = "GO conf"
 	const conferrenceTIckets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
+	//slice aka List
+	var bookings []string
+	// var bookings =  []string{}
 	//auto spacing
 	//Variables: var name type = value
 
@@ -17,12 +21,61 @@ func main() {
 	fmt.Println("We have total of", conferrenceTIckets, "and remaining", remainingTickets)
 	fmt.Println("Get tickets here")
 
-	var userName string
-	var userTickets int
-	//input name
+	//Array
+	//var var_name = [size]var_type
+	//var var_name = [size]var_type{}
+	//var bookings = [50]string{}
 
-	fmt.Scan(&userName)
+	for {
+		var userName string
+		var email string
+		var userTickets uint
+		//input name
+		fmt.Print("\nEnter Usename: ")
+		fmt.Scan(&userName)
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+		fmt.Print("Enter email: ")
+		fmt.Scan(&email)
+
+		fmt.Print("Enter TIckes: ")
+		fmt.Scan(&userTickets)
+
+		isValidemail := strings.Contains(email, "@")
+
+		if !isValidemail {
+			fmt.Println("not valid email")
+			continue
+		}
+
+		if userTickets > remainingTickets {
+			fmt.Println("Oly this many remaining:", remainingTickets)
+			continue
+		}
+
+		remainingTickets = remainingTickets - userTickets
+		//bookings[0] = userName
+		bookings = append(bookings, userName)
+
+		fmt.Printf("Slice: %v\n", bookings)
+		fmt.Printf("First value: %v\n", bookings[0])
+		fmt.Printf("Slcie Type: %T\n", bookings)
+		fmt.Printf("Slcie LEngth: %v\n", len(bookings))
+
+		for index, booking := range bookings {
+			fmt.Println(index, " has", booking)
+		}
+		// _ used as blank identifier, unussed vaariables
+		// for _, booking := range bookings {
+		// 	fmt.Println(booking)
+		// }
+
+		fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+		fmt.Printf("Remaining tickets: %v", remainingTickets)
+
+		if remainingTickets == 0 {
+			fmt.Println("All booked!")
+			break
+		}
+	}
 
 }
