4b3b67c6501aa894cfbf94901acca234a9720cf6
arya2004 <arya20j4@gmail.com>
2023-12-15 14:41:41 +0530

    function

--- Project/main.go
+++ Project/main.go
@@ -5,40 +5,33 @@ import (
 	"strings"
 )
 
+//package level var. all func have access to them
+
+var conferrenceName string = "GO conf"
+
+const conferrenceTIckets int = 50
+
+var remainingTickets uint = 50
+
+// slice aka List
+var bookings []string
+
 func main() {
-	var conferrenceName string = "GO conf"
-	const conferrenceTIckets int = 50
-	var remainingTickets uint = 50
-	//slice aka List
-	var bookings []string
+
 	// var bookings =  []string{}
 	//auto spacing
 	//Variables: var name type = value
 
 	fmt.Printf("conferrenceName type %T\n", conferrenceName)
 
-	fmt.Printf("Welcome to %v booking app\n", conferrenceName)
-	fmt.Println("We have total of", conferrenceTIckets, "and remaining", remainingTickets)
-	fmt.Println("Get tickets here")
-
+	greetUser()
 	//Array
 	//var var_name = [size]var_type
 	//var var_name = [size]var_type{}
 	//var bookings = [50]string{}
 
 	for {
-		var userName string
-		var email string
-		var userTickets uint
-		//input name
-		fmt.Print("\nEnter Usename: ")
-		fmt.Scan(&userName)
-
-		fmt.Print("Enter email: ")
-		fmt.Scan(&email)
-
-		fmt.Print("Enter TIckes: ")
-		fmt.Scan(&userTickets)
+		userName, email, userTickets := getUserInput()
 
 		isValidemail := strings.Contains(email, "@")
 
@@ -52,25 +45,19 @@ func main() {
 			continue
 		}
 
-		remainingTickets = remainingTickets - userTickets
-		//bookings[0] = userName
-		bookings = append(bookings, userName)
-
 		fmt.Printf("Slice: %v\n", bookings)
 		fmt.Printf("First value: %v\n", bookings[0])
 		fmt.Printf("Slcie Type: %T\n", bookings)
 		fmt.Printf("Slcie LEngth: %v\n", len(bookings))
 
-		for index, booking := range bookings {
-			fmt.Println(index, " has", booking)
-		}
+		bookTickets(userTickets, userName)
+
 		// _ used as blank identifier, unussed vaariables
 		// for _, booking := range bookings {
 		// 	fmt.Println(booking)
 		// }
 
-		fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
-		fmt.Printf("Remaining tickets: %v", remainingTickets)
+		printFirstNames()
 
 		if remainingTickets == 0 {
 			fmt.Println("All booked!")
@@ -78,4 +65,44 @@ func main() {
 		}
 	}
 
+}
+
+func greetUser() {
+	fmt.Printf("Welcome to %v booking app\n", conferrenceName)
+	fmt.Println("We have total of", conferrenceTIckets, "and remaining", remainingTickets)
+	fmt.Println("Get tickets here")
+
+}
+
+func printFirstNames() {
+	for index, booking := range bookings {
+		fmt.Println(index, " has", booking)
+	}
+}
+
+func getUserInput() (string, string, uint) {
+	var userName string
+	var email string
+	var userTickets uint
+	//input name
+	fmt.Print("\nEnter Usename: ")
+	fmt.Scan(&userName)
+
+	fmt.Print("Enter email: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter TIckes: ")
+	fmt.Scan(&userTickets)
+
+	return userName, email, userTickets
+
+}
+
+func bookTickets(userTickets uint, userName string) {
+	remainingTickets = remainingTickets - userTickets
+	//bookings[0] = userName
+	bookings = append(bookings, userName)
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Remaining tickets: %v", remainingTickets)
 }
